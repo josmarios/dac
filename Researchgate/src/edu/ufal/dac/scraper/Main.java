6c3c46059cd575513c8348b879a5e6f1e5fc682e
yvesbastos <raphael.bastos@me.com>
2016-03-07 07:56:21 -0300

    INPUT_FILE and OUTPUT_DIR adaptations
    Paths to input and output files adapted in Main() to work in different
    computers

--- Researchgate/src/edu/ufal/dac/scraper/Main.java
+++ Researchgate/src/edu/ufal/dac/scraper/Main.java
@@ -5,10 +5,12 @@ public class Main {
 	private static final String SEARCH_ENGINE = "http://search.yahoo.com/search?p=";
 
 	// File containing a list of authors from RBIE
-	private static final String INPUT_FILE = "/home/josmario/AUTHORS_RBIE.in";
+	//private static final String INPUT_FILE = "/home/josmario/AUTHORS_RBIE.in";
+	private static final String INPUT_FILE = "data/AUTHORS_RBIE.in";
 
 	// JSON file to place extracted data from Researchgate
-	private static final String OUTPUT_DIR = "/home/josmario/RESEARCHGATE_AUTHORS/";
+	//private static final String OUTPUT_DIR = "/home/josmario/RESEARCHGATE_AUTHORS/";
+	private static final String OUTPUT_DIR = "data/RESEARCHGATE_AUTHORS/";
 
 	public static void main(String[] args) {
 
