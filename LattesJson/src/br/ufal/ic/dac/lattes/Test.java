64b12c18ec920e8529d0dc58a34ab9ce1aeb289e
Josmario Albuquerque <josmarios.silva@gmail.com>
2016-06-10 21:32:18 -0300

    XSLT API added.

---
+++ LattesJson/src/br/ufal/ic/dac/lattes/Test.java
@@ -0,0 +1,20 @@
+package br.ufal.ic.dac.lattes;
+
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		String input = "/home/josmario/xml-data/curriculo.xml";
+		String model = "/home/josmario/xml-data/template.xsl";
+		String output = "/home/josmario/xml-data/output";
+		
+		try {
+			XSLTransform.transform(input, model, output);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
